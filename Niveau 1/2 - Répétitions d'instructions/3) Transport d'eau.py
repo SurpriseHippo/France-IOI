13c6a590d406e4ab15a46b2694168d8df6e119be
SurpriseHippo <103134678+SurpriseHippo@users.noreply.github.com>
2023-06-02 15:38:55 +0200

    ok

---
+++ Niveau 1/2 - Répétitions d'instructions/3) Transport d'eau.py
@@ -0,0 +1,10 @@
+from robot import * 
+
+gauche()
+gauche()
+print("Bonjour, laissez-moi vous aider")
+ramasser()
+for i in range(32):
+   droite()
+   
+deposer()
---
+++ Niveau 1/2 - Répétitions d'instructions/6) Page d'écriture.py
@@ -0,0 +1,9 @@
+for i in range(29):
+   print("a_", end = "")
+print("a_")
+for i in range(29):
+   print("b_", end = "")
+print("b_")
+for i in range(29):
+   print("c_", end = "")  
+print("c_") 
